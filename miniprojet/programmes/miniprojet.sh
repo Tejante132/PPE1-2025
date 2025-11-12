869c9cacd5417ff39a4d03f7d62442f6e41d4948
Clotilde <clotildegg@gmail.com>
2025-11-12 09:43:32 +0100

    quelques modifications et améliorations au miniprojet-3 (mise en forme, 'N/A')

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -4,7 +4,7 @@
 if [ $# -ne 1 ] # teste si nb d'argument différent de 1
 then
 	echo "Donner un paramètre (chemin vers fichier d'URLs)"
-	exit # fin de programme
+	exit 1 # fin de programme
 fi
 
 N=0 	# compteur d'URLs
@@ -14,9 +14,17 @@ URLS=$1
 fichier=tableaux/tableau-fr.html
 touch ${fichier}
 
-echo -e "<html><head><title>Tableau d'URLs</title><meta charset="UTF-8" /></head>" > ${fichier}
-echo -e "<body><table><tr><th>N</th><th>URL</th><th>Statut HTTP</th><th>Encodage</th><th>Nb mots</th></tr>" > ${fichier}
-# echo -e "N\tURL\tStatut HTTP\tEncodage\tNb mots" > ${fichier}
+echo -e "
+<html>
+<head>
+	<title>Tableau d'URLs</title>
+	<meta charset="UTF-8" />
+</head>" > ${fichier}
+
+echo -e "
+<body>
+	<table>
+		<tr><th>N</th><th>URL</th><th>Statut HTTP</th><th>Encodage</th><th>Nb mots</th></tr>" > ${fichier}
 
 while read -r line;
 do
@@ -30,15 +38,35 @@ do
 	# code de statut
 # 	CODE_HTTP=$(echo "${METADONNEES}" | head -n 1 | awk '{print $2}') # lit la 1ère ligne
 	CODE_HTTP=$(echo "${METADONNEES}" | head -n 1 | grep -oP " \K\d{3}")
+	if [ -z "$CODE_HTTP" ]
+	then
+		CODE_HTTP="000"
+	fi
 
 	# encodage : on fait une regex
 	ENCODING=$(echo "${METADONNEES}" | grep -i "content-type" | grep -oP "charset=\K[^; ]+")
 
+	if [ -z "$ENCODING" ]
+		# écrire truc à faire si pas d'encodate donné
+	then
+		ENCODING="N/A"
+	fi
+
 	NB_MOTS=$(lynx -dump -nolist ${line} | wc -w)
 
 	# on affiche les données extraites espacées par des tabulations
-	echo -e "<tr><td>${N}</td><td>${line}</td><td>${CODE_HTTP}</td><td>${ENCODING}</td><td>${NB_MOTS}</td></tr>" >> ${fichier}
+	echo -e "
+		<tr>
+			<td>${N}</td>
+			<td>${line}</td>
+			<td>${CODE_HTTP}</td>
+			<td>${ENCODING}</td>
+			<td>${NB_MOTS}</td>
+		</tr>" >> ${fichier}
 
 done < ${URLS};
 
-echo -e "</table></body></html>" >> ${fichier}
+echo -e "
+	</table>
+</body>
+</html>" >> ${fichier}
